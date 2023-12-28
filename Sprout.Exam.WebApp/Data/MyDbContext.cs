a93f0252f118ba0ef0bea7c49e7942216e227a39
Vince <banongofficial28@gmail.com>
2023-12-29 02:47:25 +0800

    Configured DbContext and Employee Model

---
+++ Sprout.Exam.WebApp/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sprout.Exam.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sprout.Exam.WebApp.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Sprout.Exam.WebApp/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sprout.Exam.WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Fullname { get; set; }
+        public string TIN { get; set; }
+        public int EmployeeTypeId { get; set; }
+    }
+}
