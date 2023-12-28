aa7babd1f811e365ea7b8e03ff9eecce6bd5b985
Vince <banongofficial28@gmail.com>
2023-12-29 02:58:58 +0800

    Added Migration to Database for Employee

---
+++ Sprout.Exam.WebApp/Migrations/20231228185721_addEmployeeToDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sprout.Exam.WebApp.Migrations
+{
+    public partial class addEmployeeToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Birthdate = table.Column<DateTime>(type: "date", nullable: false),
+                    Fullname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TIN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- Sprout.Exam.WebApp/Models/Employee.cs
+++ Sprout.Exam.WebApp/Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,13 @@ namespace Sprout.Exam.WebApp.Models
 {
     public class Employee
     {
+        [Key]
         public int Id { get; set; }
+        [Column(TypeName = "date")]
         public DateTime Birthdate { get; set; }
+        [Required]
         public string Fullname { get; set; }
+        [Required]
         public string TIN { get; set; }
         public int EmployeeTypeId { get; set; }
     }
